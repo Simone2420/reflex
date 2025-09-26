0c0a97916615733e09c99554a3773fc2d5a4ee7f
simone <blasssimom@gmail.com>
2025-09-25 19:45:31 -0500

    implementacion de fastapi
    Se implementa un build sh y fastapi en el backend

--- pagina_web/campos_omar/campos_omar.py
+++ pagina_web/campos_omar/campos_omar.py
@@ -1,9 +1,17 @@
 """Welcome to Reflex! This file outlines the steps to create a basic app."""
 
 import reflex as rx
+from fastapi import FastAPI
+
 from campos_omar.components.nav_bar import *
 from campos_omar.components.styles import *
 from rxconfig import config
+
+api = FastAPI()
+
+@api.get("/")
+def get_data():
+    return {"mensaje": "¡Hola desde la API Reflex en el puerto 800!"}
 
 
 class State(rx.State):
@@ -22,5 +30,5 @@ def index() -> rx.Component:
     
 
 
-app = rx.App(style=style)
+app = rx.App(style=style, api_transformer=api)
 app.add_page(index)
