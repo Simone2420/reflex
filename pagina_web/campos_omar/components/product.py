3f52672c2b9a96adb6b190817c6c3d3521d947f1
simone <blasssimom@gmail.com>
2025-09-27 09:17:49 -0500

    assets de productos

--- pagina_web/campos_omar/components/product.py
+++ pagina_web/campos_omar/components/product.py
@@ -5,7 +5,7 @@ def product(name: str, image: str) -> rx.Component:
         rx.image(src=image, alt=name, width="150px", height="150px"),
         rx.text(name, font_size="18px", font_weight="bold", margin_top="10px"),
         border="1px solid #ccc",
-        border_radius="10px",
+        border_radius="100px",
         padding="10px",
         box_shadow="0 4px 8px rgba(0, 0, 0, 0.1)",
         background_color="#fff",
