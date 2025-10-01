0684da0d20d016443719992d02fe5706e2c18d30
Blass Simón Muñoz Monterrosa <blasssimom@gmail.com>
2025-10-01 07:25:57 -0500

    cambio temp

--- pagina_web/campos_omar/components/input.py
+++ pagina_web/campos_omar/components/input.py
@@ -1,19 +1,20 @@
 import reflex as rx
 
-class InputState(rx.State):
-    is_focused: bool = False
-
-    def set_focus(self, focused: bool):
-        self.is_focused = focused
 
 
 def input_component(placeholder: str, width: str | None):
     
-    return rx.input(
-        placeholder=placeholder,
+    return rx.box( 
+        # rx.text(placeholder, 
+        #         position="absolute", 
+        #         position_top="70%",
+        #         position_left="20px",
+        #         font_size="16px",
+        #     ),
+        rx.input(
+        placeholder= f"{placeholder}",
         width=width,
-        on_focus=lambda: InputState.set_focus(True),
-        on_blur=lambda: InputState.set_focus(False),
+        
         padding="10px",
         height="50px",
         margin_bottom="15px",
@@ -21,14 +22,12 @@ def input_component(placeholder: str, width: str | None):
         border_radius="5px",
         font_size="16px",
         class_name="input",
-    )
+    ))
 def textarea_component(placeholder: str, width: str | None, height: str | None):
     return rx.input(
         placeholder=placeholder,
         width=width,
         height=height,
-        on_focus=lambda: InputState.set_focus(True),
-        on_blur=lambda: InputState.set_focus(False),
         padding="10px",
         margin_bottom="15px",
         border="1px solid #ccc",
